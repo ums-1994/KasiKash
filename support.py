119b2f9caf735fa8fa72c078941704674bfc4781
ums-1994 <umsibanda@GoRent-PC>
2025-06-06 10:03:32 +0200

    Pricing and member contribution

--- support.py
+++ support.py
@@ -17,11 +17,11 @@ warnings.filterwarnings("ignore")
 load_dotenv()
 
 # Database connection parameters
-DB_NAME = os.getenv('DB_NAME', 'kasikash')
-DB_USER = os.getenv('DB_USER', 'postgres')
-DB_PASSWORD = os.getenv('DB_PASSWORD', 'postgres')
-DB_HOST = os.getenv('DB_HOST', 'localhost')
-DB_PORT = os.getenv('DB_PORT', '5432')
+DB_NAME = 'kasikash'
+DB_USER = 'postgres'
+DB_PASSWORD = '12345'
+DB_HOST = 'localhost'
+DB_PORT = '5432'
 
 @contextmanager
 def db_connection():
