dc565a6fa359e46bf08812a8612751b90d7e0e64
Unathi <umsibanda.1994@gmail.com>
2025-07-24 15:56:24 +0200

    Use DATABASE_URL for database connection

--- backend/main.py
+++ backend/main.py
@@ -102,13 +102,7 @@ if not firebase_admin._apps:
 
 def get_db():
     try:
-        conn = psycopg2.connect(
-            dbname=os.getenv('DB_NAME'),
-            user=os.getenv('DB_USER'),
-            password=os.getenv('DB_PASSWORD'),
-            host=os.getenv('DB_HOST'),
-            port=os.getenv('DB_PORT')
-        )
+        conn = psycopg2.connect(os.environ["DATABASE_URL"])
         return conn
     except Exception as e:
         print(f"Database connection error: {str(e)}")
